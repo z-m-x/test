653564dd26a63e805421beefeb8fd642aa719478
z-m-x <zhangmingxuan@xtoneict.com>
2020-06-24 12:56:46 +0800

    修改面包屑逻辑

--- codedemo/crumbs.js
+++ codedemo/crumbs.js
@@ -2,22 +2,33 @@
 export default {
     namespaced: true,
     state: {
-        crumbsList: [{ name: '当前页面名称', route: '当前页面路由' },{ name: '当前页面名称1', route: '当前页面路由1' }]//面包屑路由数组 {name:'当前页面名称',route:'当前页面路由'}
+        crumbsList: []//面包屑路由数组 {name:'当前页面名称',route:'当前页面路由'}
     },
     mutations: {
         setCrumbsList(state, params) { //push跳转前路由到面包屑数组中
             /* 判断当前路由是否存在，存在表示没有通过面包屑一层一层返回或前进，即把此路由后面所有的元素删掉 */
             let crumbsLists = Object.values(state.crumbsList)
-            console.log('面包屑', Object.values(state.crumbsList))
+            let deepCloneCrumbs=JSON.stringify(state.crumbsList)//clone用于不删除state，因为slice会改变原数组，每次循环索引有变化
+            deepCloneCrumbs=JSON.parse(deepCloneCrumbs)
             let flag = false
             let index//记录存在的索引
             crumbsLists.forEach((i, ind) => {
+                console.log(i.name == params.name)
                 i.name == params.name ? (flag = true, index = ind) : ''
             });
             if (flag) {
-                state.crumbsList.splice(index+1, state.crumbsList.length- (index+1))//+1是从哪里开始删除，只能从当前索引下一个索引开始删除，并且删除数组索引元素个数=数组长度-当前存在的元素位置，元素索引从0开始
+                for (let i = 0; i < state.crumbsList.length; i++) {
+                    if(index<i){
+                        for (let j = 0; j < deepCloneCrumbs.length; j++) {
+                            if((state.crumbsList)[i].name==deepCloneCrumbs[j].name){
+                                deepCloneCrumbs.splice(j, 1)
+                            }
+                        }
+                    }
+                }
+                state.crumbsList=  deepCloneCrumbs//删除之后的数据赋值给state
             } else {
-              state.crumbsList.push(params)
+                state.crumbsList.push(params)
             }
         },
     }
