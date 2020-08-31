2e60207dbdf56b9b19b4b45232203d6f7ada4a1b
z-m-x <>
2020-08-31 13:32:29 +0800

    防抖节流函数更新

--- codedemo/util.debounce.throttle.js
+++ codedemo/util.debounce.throttle.js
@@ -1,3 +1,5 @@
+/* 在vue中使用 timer定时器对象需要定义在data或者防抖节流函数外 */
+
 /* 防抖函数  vue timer必须定义在data中*/
  export  function  debounce(fn, wait = 1000) {
      let timer=null
@@ -9,14 +11,27 @@
       };
     }
 
-/* 截留函数 */
+/* 节流函数 */
 
 export function throttle(fn, wait = 1000) {
-    let timer=0
+    let lastTime=0
     return (...args)=>{
         let now =new Date().getTime()
-        if (now-timer>wait){
+        if (now-lastTime>wait){
             fn.apply(this,args)
+			lastTime=now
         }
+    }
+}
+/* 首次不执行 */
+
+export function throttle(fn, wait = 1000) {
+	let timer=null
+    return (...args)=>{
+		if(timer) return
+		timer=setTimeout(()=>{
+			 fn.apply(this,args)
+			 timer=null
+		},wait)
     }
 }
